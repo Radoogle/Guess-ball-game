83c7382b6b4e18c731707ad40d265400949b4705
Radu H <52208792+Radoogle@users.noreply.github.com>
2020-03-19 17:58:15 +0200

    more
    more development

--- javascript.js
+++ javascript.js
@@ -3,24 +3,30 @@ var slider = document.getElementById("range"),
     outputB = document.getElementById("outputBlue"),
     outputR = document.getElementById("outputRed"),
     inputBalls = document.getElementById("input-all-balls"),
-    totalBalls = inputBalls.value;
+    totalBalls = inputBalls.value,
+    gameOn = false;
 
 slider.max = totalBalls;
 slider.value = totalBalls / 2;
-UpdateRatio();
+updateRatio();
+
+if (!gameOn) {
+    document.getElementById("choose-container").style.display = "none";
+}
+
 
 inputBalls.oninput = function () {
     totalBalls = inputBalls.value;
     slider.max = totalBalls;
     slider.value = totalBalls / 2;
-    UpdateRatio();
+    updateRatio();
 };
 
 slider.oninput = function () {
-    UpdateRatio();
+    updateRatio();
 };
 
-function OpenCloseAboutWindow() {
+function openCloseAboutWindow() {
     let ifr = document.getElementById("about-frame"),
         main = document.getElementById("main"),
         d = document.getElementById("about-frame").style.display;
@@ -33,7 +39,35 @@ function OpenCloseAboutWindow() {
     }
 };
 
-function UpdateRatio() {
+function updateRatio() {
     outputB.innerHTML = slider.value;
     outputR.innerHTML = totalBalls - slider.value;
 };
+
+function randomColor(min = 1, max = 10) {
+    let color = Math.floor(Math.random() * (max - min + 1) + min);
+    if (color < 5) 
+        return "blue";
+    else return "red";
+}
+
+function go() {
+    let total = totalBalls;
+    let color;
+    gameOn = true;
+    document.getElementById("choose-container").style.display = "block";
+    while (total-- > 0) {
+        color = randomColor();
+        let form = document.getElementById("radio-choose-color");
+        form.onchange = function() {
+            let choice = form.value;
+            alert(choice);
+        }
+        
+        alert("next");
+        
+        }
+    alert("Game over!");
+    //gameOn = false;
+    
+}
